5e8330b13b53e36f1f5f290c045c4c02a4cdd1e9
Carlos Alberto Costa Beppler <beppler@gmail.com>
2022-01-08 22:46:20 -0300

    Remover o comando de geraçao de CAs. Para gerar uma CA deve ser gerado um CSR e depois assinar ele como uma CA.

--- simpleca.go
+++ simpleca.go
@@ -30,39 +30,6 @@ func main() {
 	app.Version = version
 	app.Commands = []*cli.Command{
 		{
-			Name:  "ca",
-			Usage: "Create a certificate authority certificate",
-			Flags: []cli.Flag{
-				&cli.StringFlag{
-					Name:  "key",
-					Usage: "RSA private key file `NAME`",
-				},
-				&cli.StringFlag{
-					Name:  "password",
-					Usage: "private key `PASSWORD`",
-				},
-				&cli.StringFlag{
-					Name:  "country",
-					Usage: "Country code for certificate request (ex. US)",
-				},
-				&cli.StringFlag{
-					Name:  "organization",
-					Usage: "Organization name for certificate",
-					Value: "ACME Development Certificate Authority",
-				},
-				&cli.IntFlag{
-					Name:  "validity",
-					Usage: "Validity time in `YEARS`",
-					Value: 10,
-				},
-				&cli.StringFlag{
-					Name:  "out",
-					Usage: "Certificate authority certificate output file `NAME`",
-				},
-			},
-			Action: genCA,
-		},
-		{
 			Name:  "csr",
 			Usage: "Create a certificate request",
 			Flags: []cli.Flag{
@@ -208,7 +175,7 @@ func main() {
 			Subcommands: []*cli.Command{
 				{
 					Name:  "ca",
-					Usage: "Sign a subordinate certificate authority request",
+					Usage: "Sign a certificate authority request",
 					Flags: []cli.Flag{
 						&cli.StringFlag{
 							Name:  "csr",
@@ -236,7 +203,7 @@ func main() {
 						},
 						&cli.IntFlag{
 							Name:  "max-path-len",
-							Usage: "Maximum number of subordinate CAs",
+							Usage: "Maximum number of subordinate CAs (-1 for no limit)",
 							Value: 0,
 						},
 						&cli.IntFlag{
@@ -387,8 +354,8 @@ func genKey(c *cli.Context) error {
 	if size < 1024 {
 		return fmt.Errorf("key size must be at least 1024 bits")
 	}
-	outFileName := c.String("out")
 	password := c.String("password")
+	outFileName := c.String("out")
 
 	privateKey, err := rsa.GenerateKey(rand.Reader, size)
 	if err != nil {
@@ -432,7 +399,6 @@ func genCSR(c *cli.Context) error {
 	if keyName == "" {
 		return fmt.Errorf("private key file name is required")
 	}
-	outFileName := c.String("out")
 	password := c.String("password")
 	country := c.String("country")
 	province := c.String("province")
@@ -441,6 +407,7 @@ func genCSR(c *cli.Context) error {
 	unit := c.String("unit")
 	commonName := c.String("cn")
 	dnsNames := c.StringSlice("dns")
+	outFileName := c.String("out")
 
 	var ipAddresses []net.IP
 	for _, item := range c.StringSlice("ip") {
@@ -535,92 +502,6 @@ func genCSR(c *cli.Context) error {
 	return nil
 }
 
-func genCA(c *cli.Context) error {
-	keyName := c.String("key")
-	if keyName == "" {
-		return fmt.Errorf("private key name is required")
-	}
-	organization := c.String("organization")
-	if organization == "" {
-		return fmt.Errorf("organization name is required")
-	}
-	validity := c.Int("validity")
-	if validity < 1 {
-		return fmt.Errorf("validity must be a positive number")
-	}
-	outFileName := c.String("out")
-	password := c.String("password")
-	country := c.String("country")
-
-	pemBytes, err := os.ReadFile(keyName)
-	if err != nil {
-		return fmt.Errorf("failed to load private key: %w", err)
-	}
-
-	keyPemBlock, _ := pem.Decode(pemBytes)
-	if keyPemBlock == nil {
-		return fmt.Errorf("failed to decode private key")
-	}
-
-	var keyBytes []byte
-	if password == "" {
-		keyBytes = keyPemBlock.Bytes
-	} else {
-		keyBytes, err = pemutil.DecryptPEMBlock(keyPemBlock, []byte(password))
-		if err != nil {
-			return fmt.Errorf("failed to decrypt private key: %w", err)
-		}
-	}
-
-	privateKey, err := x509.ParsePKCS1PrivateKey(keyBytes)
-	if err != nil {
-		return fmt.Errorf("failed to parse private key: %w", err)
-	}
-
-	serialNumber := big.NewInt(1)
-	now := time.Now()
-
-	template := x509.Certificate{
-		IsCA:                  true,
-		BasicConstraintsValid: true,
-		SerialNumber:          serialNumber,
-		Subject: pkix.Name{
-			Organization: []string{organization},
-		},
-		NotBefore: now,
-		NotAfter:  now.AddDate(validity, 0, 0),
-		KeyUsage:  x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
-	}
-
-	if country != "" {
-		template.Subject.Country = []string{country}
-	}
-
-	certBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, privateKey.Public(), privateKey)
-	if err != nil {
-		return fmt.Errorf("failed to create certificate: %w", err)
-	}
-
-	var outBuffer bytes.Buffer
-	outWriter := bufio.NewWriter(&outBuffer)
-	err = pem.Encode(outWriter, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
-	if err != nil {
-		return fmt.Errorf("failed to encode certificate: %w", err)
-	}
-	outWriter.Flush()
-
-	if outFileName == "" {
-		_, err = os.Stdout.Write(outBuffer.Bytes())
-	} else {
-		err = os.WriteFile(outFileName, outBuffer.Bytes(), 0600)
-	}
-	if err != nil {
-		return fmt.Errorf("failed to save certificate: %w", err)
-	}
-
-	return nil
-}
-
 func genCRL(c *cli.Context) error {
 	caCertName := c.String("ca-cert")
 	if caCertName == "" {
@@ -630,13 +511,13 @@ func genCRL(c *cli.Context) error {
 	if validity < 0 {
 		return fmt.Errorf("validity must be a positive number")
 	}
-	outFileName := c.String("out")
 	caKeyName := c.String("ca-key")
 	if caKeyName == "" {
 		caKeyName = strings.TrimSuffix(caCertName, filepath.Ext(caCertName)) + ".key"
 	}
 	caPassword := c.String("ca-password")
 	certNames := c.StringSlice("cert")
+	outFileName := c.String("out")
 
 	pemBytes, err := os.ReadFile(caCertName)
 	if err != nil {
@@ -769,6 +650,7 @@ func genPkcs(c *cli.Context) error {
 	password := c.String("password")
 	caCertNames := c.StringSlice("ca-cert")
 	outFileName := c.String("out")
+
 	certPemBytes, err := os.ReadFile(certName)
 	if err != nil {
 		return fmt.Errorf("failed to load certificate: %w", err)
@@ -844,23 +726,113 @@ func genPkcs(c *cli.Context) error {
 	return nil
 }
 
-func signRequest(c *cli.Context, configure func(*x509.Certificate) error) error {
+func signCA(c *cli.Context) error {
+	maxPathLen := c.Int("max-path-len")
+	if maxPathLen < 0 {
+		return fmt.Errorf("path length must be equal or greater than 0")
+	}
+	validity := c.Int("validity")
+	if validity < 1 {
+		return fmt.Errorf("validity must be a positive number")
+	}
+
+	configure := func(template *x509.Certificate) error {
+		template.IsCA = true
+		template.SerialNumber = big.NewInt(1)
+		template.MaxPathLen = maxPathLen
+		template.MaxPathLenZero = c.IsSet("max-path-len")
+		template.BasicConstraintsValid = true
+		template.NotAfter = template.NotBefore.AddDate(validity, 0, 0)
+		template.KeyUsage = x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign | x509.KeyUsageCRLSign
+		template.DNSNames = nil
+		template.IPAddresses = nil
+		template.EmailAddresses = nil
+		return nil
+	}
+
+	return signRequest(c, true, configure)
+}
+
+func signCode(c *cli.Context) error {
+	validity := c.Int("validity")
+	if validity < 1 {
+		return fmt.Errorf("validity must be a positive number")
+	}
+
+	configure := func(template *x509.Certificate) error {
+		template.NotAfter = template.NotBefore.AddDate(validity, 0, 0)
+		template.KeyUsage = x509.KeyUsageDigitalSignature
+		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageCodeSigning}
+		template.DNSNames = nil
+		template.IPAddresses = nil
+		template.EmailAddresses = nil
+		return nil
+	}
+
+	return signRequest(c, false, configure)
+}
+
+func signServer(c *cli.Context) error {
+	validity := c.Int("validity")
+	if validity < 1 {
+		return fmt.Errorf("validity must be a positive number")
+	}
+
+	configure := func(template *x509.Certificate) error {
+		template.NotAfter = template.NotBefore.AddDate(validity, 0, 0)
+		template.KeyUsage = x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageKeyAgreement
+		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}
+		template.EmailAddresses = nil
+		if len(template.DNSNames) == 0 {
+			template.DNSNames = []string{template.Subject.CommonName}
+		}
+		return nil
+	}
+
+	return signRequest(c, false, configure)
+}
+
+func signUser(c *cli.Context) error {
+	validity := c.Int("validity")
+	if validity < 1 {
+		return fmt.Errorf("validity must be a positive number")
+	}
+
+	configure := func(template *x509.Certificate) error {
+		template.NotAfter = template.NotBefore.AddDate(validity, 0, 0)
+		template.KeyUsage = x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment | x509.KeyUsageContentCommitment
+		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageEmailProtection}
+		template.DNSNames = nil
+		template.IPAddresses = nil
+		return nil
+	}
+
+	return signRequest(c, false, configure)
+}
+
+func signRequest(c *cli.Context, allowSelfSign bool, configure func(*x509.Certificate) error) error {
 	csrName := c.String("csr")
 	if csrName == "" {
 		return fmt.Errorf("certificate request file name is required")
 	}
 	caCertName := c.String("ca-cert")
-	if caCertName == "" {
+	if !allowSelfSign && caCertName == "" {
 		return fmt.Errorf("certificate authority certificate file name is required")
 	}
-	outFileName := c.String("out")
 	caKeyName := c.String("ca-key")
 	if caKeyName == "" {
-		caKeyName = strings.TrimSuffix(caCertName, filepath.Ext(caCertName)) + ".key"
+		if caCertName != "" {
+			caKeyName = strings.TrimSuffix(caCertName, filepath.Ext(caCertName)) + ".key"
+		} else if csrName != "" {
+			caKeyName = strings.TrimSuffix(csrName, filepath.Ext(csrName)) + ".key"
+		} else {
+			return fmt.Errorf("certificate authority private key file name is required")
+		}
 	}
 	caPassword := c.String("ca-password")
 	crls := c.StringSlice("crl")
 	ocsps := c.StringSlice("ocsp")
+	outFileName := c.String("out")
 
 	pemBytes, err := os.ReadFile(csrName)
 	if err != nil {
@@ -879,21 +851,6 @@ func signRequest(c *cli.Context, configure func(*x509.Certificate) error) error 
 
 	if request.Subject.CommonName == "" {
 		return fmt.Errorf("common name field is required")
-	}
-
-	pemBytes, err = os.ReadFile(caCertName)
-	if err != nil {
-		return fmt.Errorf("failed to load certificate authority certificate: %w", err)
-	}
-
-	certPemBlock, _ := pem.Decode(pemBytes)
-	if certPemBlock == nil {
-		return fmt.Errorf("failed to decode certificate authority certificate")
-	}
-
-	caCert, err := x509.ParseCertificate(certPemBlock.Bytes)
-	if err != nil {
-		return fmt.Errorf("failed to parse certificate authority certificate: %w", err)
 	}
 
 	pemBytes, err = os.ReadFile(caKeyName)
@@ -921,80 +878,88 @@ func signRequest(c *cli.Context, configure func(*x509.Certificate) error) error 
 		return fmt.Errorf("failed to parse certificate authority private key: %w", err)
 	}
 
-	crlsName := strings.TrimSuffix(caKeyName, filepath.Ext(caKeyName)) + ".crls.txt"
-	if file, err := os.Open(crlsName); err == nil {
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			crls = append(crls, scanner.Text())
-		}
-		file.Close()
-	}
-
-	ocspsName := strings.TrimSuffix(caKeyName, filepath.Ext(caKeyName)) + ".ocsps.txt"
-	if file, err := os.Open(ocspsName); err == nil {
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			ocsps = append(ocsps, scanner.Text())
-		}
-	}
-
 	serialNumber := big.NewInt(time.Now().UnixNano())
 	now := time.Now()
-
-	if now.After(caCert.NotAfter) {
-		return fmt.Errorf("CA certificate (%s) is expired", caCert.Subject)
-	}
 
 	template := x509.Certificate{
 		IsCA:                  false,
 		BasicConstraintsValid: true,
 		SerialNumber:          serialNumber,
 		Subject: pkix.Name{
-			CommonName: request.Subject.CommonName,
+			CommonName:         request.Subject.CommonName,
+			Country:            request.Subject.Country,
+			Organization:       request.Subject.Organization,
+			OrganizationalUnit: request.Subject.OrganizationalUnit,
 		},
-		NotBefore: now,
-		NotAfter:  now,
+		DNSNames:              request.DNSNames,
+		IPAddresses:           request.IPAddresses,
+		EmailAddresses:        request.EmailAddresses,
+		CRLDistributionPoints: crls,
+		OCSPServer:            ocsps,
+		NotBefore:             now,
+		NotAfter:              now,
 	}
 
-	if len(request.Subject.Country) != 0 {
-		template.Subject.Country = request.Subject.Country
-	} else if len(caCert.Subject.Country) != 0 {
-		template.Subject.Country = caCert.Subject.Country
-	}
-	if len(request.Subject.Organization) != 0 {
-		template.Subject.Organization = request.Subject.Organization
-	} else if len(caCert.Subject.Organization) != 0 {
-		template.Subject.Organization = caCert.Subject.Organization
-	}
-	if len(request.Subject.OrganizationalUnit) != 0 {
-		template.Subject.OrganizationalUnit = request.Subject.OrganizationalUnit
-	}
-	if len(request.DNSNames) != 0 {
-		template.DNSNames = request.DNSNames
-	}
-	if len(request.IPAddresses) != 0 {
-		template.IPAddresses = request.IPAddresses
-	}
-	if len(request.EmailAddresses) != 0 {
-		template.EmailAddresses = request.EmailAddresses
-	}
-	if len(crls) != 0 {
-		template.CRLDistributionPoints = crls
-	} else if len(caCert.CRLDistributionPoints) != 0 {
-		template.CRLDistributionPoints = caCert.CRLDistributionPoints
-	}
-	if len(ocsps) != 0 {
-		template.OCSPServer = ocsps
-	} else if len(caCert.OCSPServer) != 0 {
-		template.OCSPServer = caCert.OCSPServer
+	var caCert *x509.Certificate
+	if caCertName == "" {
+		caCert = &template
+	} else {
+		pemBytes, err = os.ReadFile(caCertName)
+		if err != nil {
+			return fmt.Errorf("failed to load certificate authority certificate: %w", err)
+		}
+
+		certPemBlock, _ := pem.Decode(pemBytes)
+		if certPemBlock == nil {
+			return fmt.Errorf("failed to decode certificate authority certificate")
+		}
+
+		caCert, err = x509.ParseCertificate(certPemBlock.Bytes)
+		if err != nil {
+			return fmt.Errorf("failed to parse certificate authority certificate: %w", err)
+		}
+
+		if now.After(caCert.NotAfter) {
+			return fmt.Errorf("CA certificate (%s) is expired", caCert.Subject)
+		}
+
+		if template.NotAfter.After(caCert.NotAfter) {
+			template.NotAfter = caCert.NotAfter
+		}
+
+		if len(template.Subject.Country) == 0 && len(caCert.Subject.Country) != 0 {
+			template.Subject.Country = caCert.Subject.Country
+		}
+		if len(template.Subject.Organization) == 0 && len(caCert.Subject.Organization) != 0 {
+			template.Subject.Organization = caCert.Subject.Organization
+		}
+		if len(template.CRLDistributionPoints) == 0 && len(caCert.CRLDistributionPoints) != 0 {
+			template.CRLDistributionPoints = append(template.CRLDistributionPoints, caCert.CRLDistributionPoints...)
+		}
+		if len(template.OCSPServer) == 0 && len(caCert.OCSPServer) != 0 {
+			template.OCSPServer = append(template.OCSPServer, caCert.OCSPServer...)
+		}
+
+		crlsName := strings.TrimSuffix(caKeyName, filepath.Ext(caKeyName)) + ".crls.txt"
+		if file, err := os.Open(crlsName); err == nil {
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				template.CRLDistributionPoints = append(template.CRLDistributionPoints, scanner.Text())
+			}
+			file.Close()
+		}
+
+		ocspsName := strings.TrimSuffix(caKeyName, filepath.Ext(caKeyName)) + ".ocsps.txt"
+		if file, err := os.Open(ocspsName); err == nil {
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				template.CRLDistributionPoints = append(template.CRLDistributionPoints, scanner.Text())
+			}
+		}
 	}
 
 	if err := configure(&template); err != nil {
 		return fmt.Errorf("failed to configure certificate policies: %w", err)
-	}
-
-	if template.NotAfter.After(caCert.NotAfter) {
-		return fmt.Errorf("CA certificate will be expired before certificate expiration")
 	}
 
 	certBytes, err := x509.CreateCertificate(rand.Reader, &template, caCert, request.PublicKey, caKey)
@@ -1020,87 +985,4 @@ func signRequest(c *cli.Context, configure func(*x509.Certificate) error) error 
 	}
 
 	return nil
-}
-
-func signCA(c *cli.Context) error {
-	maxPathLen := c.Int("max-path-len")
-	if maxPathLen < 0 {
-		return fmt.Errorf("path length must be equal or greater than 0")
-	}
-	validity := c.Int("validity")
-	if validity < 1 {
-		return fmt.Errorf("validity must be a positive number")
-	}
-
-	configure := func(template *x509.Certificate) error {
-		template.IsCA = true
-		template.MaxPathLen = maxPathLen
-		template.MaxPathLenZero = maxPathLen == 0
-		template.BasicConstraintsValid = true
-		template.NotAfter = template.NotBefore.AddDate(validity, 0, 0)
-		template.KeyUsage = x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign | x509.KeyUsageCRLSign
-		template.DNSNames = nil
-		template.IPAddresses = nil
-		template.EmailAddresses = nil
-		return nil
-	}
-
-	return signRequest(c, configure)
-}
-
-func signCode(c *cli.Context) error {
-	validity := c.Int("validity")
-	if validity < 1 {
-		return fmt.Errorf("validity must be a positive number")
-	}
-
-	configure := func(template *x509.Certificate) error {
-		template.NotAfter = template.NotBefore.AddDate(validity, 0, 0)
-		template.KeyUsage = x509.KeyUsageDigitalSignature
-		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageCodeSigning}
-		template.DNSNames = nil
-		template.IPAddresses = nil
-		template.EmailAddresses = nil
-		return nil
-	}
-
-	return signRequest(c, configure)
-}
-
-func signServer(c *cli.Context) error {
-	validity := c.Int("validity")
-	if validity < 1 {
-		return fmt.Errorf("validity must be a positive number")
-	}
-
-	configure := func(template *x509.Certificate) error {
-		template.NotAfter = template.NotBefore.AddDate(validity, 0, 0)
-		template.KeyUsage = x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageKeyAgreement
-		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}
-		template.EmailAddresses = nil
-		if len(template.DNSNames) == 0 {
-			template.DNSNames = []string{template.Subject.CommonName}
-		}
-		return nil
-	}
-
-	return signRequest(c, configure)
-}
-
-func signUser(c *cli.Context) error {
-	validity := c.Int("validity")
-	if validity < 1 {
-		return fmt.Errorf("validity must be a positive number")
-	}
-
-	configure := func(template *x509.Certificate) error {
-		template.NotAfter = template.NotBefore.AddDate(validity, 0, 0)
-		template.KeyUsage = x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment | x509.KeyUsageContentCommitment
-		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageEmailProtection}
-		template.DNSNames = nil
-		template.IPAddresses = nil
-		return nil
-	}
-
-	return signRequest(c, configure)
 }
